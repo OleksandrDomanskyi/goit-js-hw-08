b10bed31bc6e56eca80eb1b8287c4fe176fca358
Oleksandr Domanskiy <domanskyioleksandr@gmail.com>
2022-04-25 23:35:01 +0300

    task 2 solution

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,20 @@
+import Player from '@vimeo/player';
 
+import throttle from 'lodash.throttle';
+
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+
+const LOCAL_STORAGE_KEY = "videoplayer-current-time";
+const passedTime = localStorage.getItem(LOCAL_STORAGE_KEY);
+
+if (passedTime) {
+    player.setCurrentTime(passedTime);
+};
+
+player.on('timeupdate', throttle(onTimeUpdate, 1000));
+
+function onTimeUpdate(event) {
+    const currentTime = event.seconds;
+    localStorage.setItem(LOCAL_STORAGE_KEY, currentTime);
+};

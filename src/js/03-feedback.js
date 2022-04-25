e1bf89504f428ef4fb35a79b40b65152bdb37784
Oleksandr Domanskiy <domanskyioleksandr@gmail.com>
2022-04-26 02:51:49 +0300

    Task 3 solution added

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,45 @@
+import throttle from 'lodash.throttle';
 
+const ref = {
+    form: document.querySelector('.feedback-form'),
+};
+
+const LOCAL_STORAGE_KEY = "feedback-form-state";
+
+addLocalStorageInputValue();
+
+ref.form.addEventListener('input', throttle(onFormInput, 500));
+ref.form.addEventListener('submit', onFormSubmit);
+
+const formData = {
+    email: ref.form.email.value,
+    message: ref.form.message.value,    
+    };
+
+function onFormInput(event) {
+    formData[event.target.name] = event.target.value;
+    const stringifiedData = JSON.stringify(formData);
+    localStorage.setItem(LOCAL_STORAGE_KEY, stringifiedData);
+};
+
+function onFormSubmit(event) {
+    event.preventDefault();
+
+    const dataSubmit = {
+        email: event.currentTarget.email.value,
+        message: event.currentTarget.message.value,
+    };
+    console.log(dataSubmit);
+
+    localStorage.removeItem(LOCAL_STORAGE_KEY);
+    event.currentTarget.reset();
+};
+
+function addLocalStorageInputValue() {
+    let storageInputValue = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+    
+    if (!storageInputValue) return;
+    
+    ref.form.elements.email.value = storageInputValue.email;        
+    ref.form.elements.message.value = storageInputValue.message;   
+};
